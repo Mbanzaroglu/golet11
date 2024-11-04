29b0d6bd1707f85a2946ce6d8e91df2114aa10f8
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-11-04 15:13:51 +0300

    Deleted the codes since ORM is no more needed

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,21 +1,8 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
-db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    # Blueprint ile rotaları import et
-    from .routes import main
-    app.register_blueprint(main)
-
-    # MySQL veritabanı bağlantısını yapılandır
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Banzaroglu196761@127.0.0.1/bp_database'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # İzleme modunu kapatarak performansı artırır
-
-    # SQLAlchemy'yi uygulama ile başlat
-    db.init_app(app)
 
     return app
