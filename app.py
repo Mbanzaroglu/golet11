1c597445a1835e4fd4d0df215f7568e5b230f536
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-11-04 17:12:54 +0300

    Added html routes

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ from backend.mysql_connector import initialize_database
 
 app = create_app()
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 # Uygulama başlatıldığında veritabanını başlat
 print("Initializing database...")
 initialize_database()  # Tabloları oluşturacak
