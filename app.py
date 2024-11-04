a37970ca2bd4e16ba0047a586de599e5175cdeba
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-11-04 14:03:49 +0300

    Imported the initialize_database function from mysql_connector.py. It will run every time the program is started.

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from app import create_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from app.mysql_connector import initialize_database
 
 app = create_app()
+
+# Uygulama başlatıldığında veritabanını başlat
+print("Initializing database...")
+initialize_database()  # Tabloları oluşturacak
 
 if __name__ == '__main__':
     app.run(debug=True)
