64cc0bc69433bb0e9d6889e21c1c992b1d723097
ahmtakin <aa.akin57@gmail.com>
2024-11-17 17:43:27 +0300

    Add html templates

--- app.py
+++ app.py
@@ -4,17 +4,13 @@ from flask import Flask, render_template
 
 app = create_app()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
 
-# Uygulama başlatıldığında veritabanını başlat
 print("Initializing database...")
 initialize_database()  # Tabloları oluşturacak
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
