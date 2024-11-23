4f891803a98a0d7fcf11559d27269ff42d928353
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-11-23 16:23:55 +0300

    Structure has been modified and Home page, Navbar and Header is styled

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 # Define a Blueprint for routes
-main = Blueprint('main', __name__)
+main_bp = Blueprint('main', __name__)
 
-@main.route('/')
+@main_bp.route('/')
 def home():
+    # session.permanent = "True" # Set session to permanent (30 minutes)
     return render_template('home.html', title="Home Page")
