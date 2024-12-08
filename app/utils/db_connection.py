ec309e696905dc7d7383400751a8b69876e470ad
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-12-08 11:29:23 +0300

    endpointlerdeki SQL sorgularından önce bağlantı kurmak için fonksiyon yazdım

---
+++ app/utils/db_connection.py
@@ -0,0 +1,29 @@
+import mysql.connector
+from contextlib import contextmanager
+
+def connect_db():
+    try:
+        conn = mysql.connector.connect(
+            host="127.0.0.1",
+            port=3306,
+            user="root",
+            password="",
+            database="bp_database",
+            ssl_disabled=True  # SSL'i devre dışı bırak
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Veritabanı bağlantı hatası: {err}")
+        return None
+
+@contextmanager
+def get_db_connection_and_cursor():
+    conn = connect_db()
+    if conn is None:
+        raise Exception("Veritabanı bağlantısı kurulamadı.")
+    cursor = conn.cursor(dictionary=True)
+    try:
+        yield conn,cursor
+    finally:
+        cursor.close()
+        conn.close()
