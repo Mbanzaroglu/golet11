8a320723bc6ac6b812c4ba5b0e56bf4ccbd96b64
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-12-08 11:31:03 +0300

    User, Favs, ve Login Route'ları yapıldı.

---
+++ app/routes/favorites.py
@@ -0,0 +1,130 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from flask_login import login_required, current_user
+from app.utils.db_connection import get_db_connection_and_cursor
+
+fav_bp = Blueprint('fav_bp', __name__, url_prefix='/favorites')
+
+@fav_bp.route('/songs')
+@login_required
+def favorite_songs():
+    selected_page = 'favorites_songs'
+    with get_db_connection_and_cursor() as cursor:
+        query = """
+        SELECT
+            bp_track.track_id,
+            bp_track.title,
+            bp_artist.artist_name
+        FROM
+            favorite_tracks
+        INNER JOIN
+            bp_track ON favorite_tracks.track_id = bp_track.track_id
+        INNER JOIN
+            artist_track ON bp_track.track_id = artist_track.track_id
+        INNER JOIN
+            bp_artist ON artist_track.artist_id = bp_artist.artist_id
+        WHERE
+            favorite_tracks.user_id = %s
+        """
+        cursor.execute(query, (current_user.id,))
+        favorite_songs_list = cursor.fetchall()
+    query = """
+    SELECT
+        bp_track.track_id,
+        bp_track.title,
+        bp_artist.artist_name
+    FROM
+        favorite_tracks
+    INNER JOIN
+        bp_track ON favorite_tracks.track_id = bp_track.track_id
+    INNER JOIN
+        artist_track ON bp_track.track_id = artist_track.track_id
+    INNER JOIN
+        bp_artist ON artist_track.artist_id = bp_artist.artist_id
+    WHERE
+        favorite_tracks.user_id = %s
+    """
+    cursor.execute(query, (current_user.id,))
+    favorite_songs_list = cursor.fetchall()
+    cursor.close()
+    return render_template('fav.html', selected_page=selected_page, songs=favorite_songs_list)
+
+@fav_bp.route('/albums')
+@login_required
+def favorite_albums():
+    selected_page = 'favorites_albums'
+    with get_db_connection_and_cursor() as cursor:
+        query = """
+        SELECT
+            bp_release.release_id,
+            bp_release.release_title,
+            bp_artist.artist_name
+        FROM
+            favorite_albums
+        INNER JOIN
+            bp_release ON favorite_albums.release_id = bp_release.release_id
+        INNER JOIN
+            artist_release ON bp_release.release_id = artist_release.release_id
+        INNER JOIN
+            bp_artist ON artist_release.artist_id = bp_artist.artist_id
+        WHERE
+            favorite_albums.user_id = %s
+        """
+        cursor.execute(query, (current_user.id,))
+        favorite_albums_list = cursor.fetchall()
+    with get_db_connection_and_cursor() as cursor:
+        query = """
+        SELECT
+            bp_release.release_id,
+            bp_release.release_title,
+            bp_artist.artist_name
+        FROM
+            favorite_albums
+        INNER JOIN
+            bp_release ON favorite_albums.release_id = bp_release.release_id
+        INNER JOIN
+            artist_release ON bp_release.release_id = artist_release.release_id
+        INNER JOIN
+            bp_artist ON artist_release.artist_id = bp_artist.artist_id
+        WHERE
+            favorite_albums.user_id = %s
+        """
+        cursor.execute(query, (current_user.id,))
+        favorite_albums_list = cursor.fetchall()
+
+    return render_template('fav.html', selected_page=selected_page, albums=favorite_albums_list)
+
+@fav_bp.route('/artists')
+@login_required
+def favorite_artists():
+    selected_page = 'favorites_artists'
+    with get_db_connection_and_cursor() as cursor:
+        query = """
+        SELECT
+            bp_artist.artist_id,
+            bp_artist.artist_name,
+            bp_artist.artist_url
+        FROM
+            favorite_artists
+        INNER JOIN
+            bp_artist ON favorite_artists.artist_id = bp_artist.artist_id
+        WHERE
+            favorite_artists.user_id = %s
+        """
+        cursor.execute(query, (current_user.id,))
+        favorite_artists_list = cursor.fetchall()
+    with get_db_connection_and_cursor() as cursor:
+        query = """
+        SELECT
+            bp_artist.artist_id,
+            bp_artist.artist_name,
+            bp_artist.artist_url
+        FROM
+            favorite_artists
+        INNER JOIN
+            bp_artist ON favorite_artists.artist_id = bp_artist.artist_id
+        WHERE
+            favorite_artists.user_id = %s
+        """
+        cursor.execute(query, (current_user.id,))
+        favorite_artists_list = cursor.fetchall()
+    return render_template('fav.html', selected_page=selected_page, artists=favorite_artists_list)
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,9 +1,95 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, request
+from app.utils.db_connection import get_db_connection_and_cursor
 
 # Define a Blueprint for routes
 main_bp = Blueprint('main_bp', __name__)
 
 @main_bp.route('/')
 def home():
-    # session.permanent = "True" # Set session to permanent (30 minutes)
-    return render_template('home.html', title="Home Page")
+    selected_page = "home"
+    return render_template('home.html', selected_page=selected_page)
+
+@main_bp.route('/songs')
+def songs():
+    selected_page = "songs"
+    # SQL query to get songs with their artists
+    with get_db_connection_and_cursor() as cursor:
+        query = """
+        SELECT
+            bp_track.track_id,
+            bp_track.title,
+            bp_artist.artist_name
+        FROM
+            bp_track
+        LEFT JOIN
+            artist_track ON bp_track.track_id = artist_track.track_id
+        LEFT JOIN
+            bp_artist ON artist_track.artist_id = bp_artist.artist_id
+        """
+        cursor.execute(query)
+        songs_list = cursor.fetchall()
+    return render_template('home.html', selected_page=selected_page, songs=songs_list)
+
+@main_bp.route('/albums')
+def albums():
+    selected_page = 'albums'
+    # SQL query to get albums with their artists
+    with get_db_connection_and_cursor() as cursor:
+        query = """
+        SELECT
+            bp_release.release_id,
+            bp_release.release_title,
+            bp_artist.artist_name
+        FROM
+            bp_release
+        LEFT JOIN
+            artist_release ON bp_release.release_id = artist_release.release_id
+        LEFT JOIN
+            bp_artist ON artist_release.artist_id = bp_artist.artist_id
+        """
+        cursor.execute(query)
+        albums_list = cursor.fetchall()
+    return render_template('home.html', selected_page=selected_page, albums=albums_list)
+
+@main_bp.route('/artists')
+def artists():
+    selected_page = 'artists'
+    # SQL query to get artist information
+    with get_db_connection_and_cursor() as cursor:
+        query = """
+        SELECT
+            artist_id,
+            artist_name,
+            artist_url
+        FROM
+            bp_artist
+        """
+        cursor.execute(query)
+        artist_list = cursor.fetchall()
+
+    return render_template('home.html', selected_page=selected_page, artists=artist_list)
+
+@main_bp.route('/search')
+def search():
+    selected_page = 'search'
+    query_param = request.args.get('q', '')
+    with get_db_connection_and_cursor() as cursor:
+        query = """
+        SELECT
+            bp_track.track_id,
+            bp_track.title,
+            bp_artist.artist_name
+        FROM
+            bp_track
+        LEFT JOIN
+            artist_track ON bp_track.track_id = artist_track.track_id
+        LEFT JOIN
+            bp_artist ON artist_track.artist_id = bp_artist.artist_id
+        WHERE
+            bp_track.title LIKE %s OR bp_artist.artist_name LIKE %s
+        """
+        search_term = f"%{query_param}%"
+        cursor.execute(query, (search_term, search_term))
+        search_results = cursor.fetchall()
+
+    return render_template('home.html', selected_page=selected_page, search_results=search_results, query=query_param)
