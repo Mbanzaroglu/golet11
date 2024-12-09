7e301e4e2d2500b132e1ca3179e806a01e0ddc51
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2024-12-09 12:07:22 +0300

    Parçaları çekmek için SQL sorgusu yazdım. Bütün fk'lar için foreign key yazdım ki tıklanan şarkı sql sorgu hatası vermesin

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -12,23 +12,42 @@ def home():
 @main_bp.route('/songs')
 def songs():
     selected_page = "songs"
+    songs_list = []
     # SQL query to get songs with their artists
-    # with get_db_connection_and_cursor() as (conn, cursor):
-    #     query = """
-    #     SELECT
-    #         bp_track.track_id,
-    #         bp_track.title,
-    #         bp_artist.artist_name
-    #     FROM
-    #         bp_track
-    #     LEFT JOIN
-    #         artist_track ON bp_track.track_id = artist_track.track_id
-    #     LEFT JOIN
-    #         bp_artist ON artist_track.artist_id = bp_artist.artist_id
-    #     """
-    #     cursor.execute(query)
-    #     songs_list = cursor.fetchall()
-    return render_template('home.html', selected_page=selected_page)
+    with get_db_connection_and_cursor() as (conn, cursor):
+        # Önce toplam kayıt sayısını al
+        cursor.execute("SELECT COUNT(*) as count FROM bp_track")
+        total_records = cursor.fetchone()['count']
+        
+        # Rastgele 50 ID oluştur
+        # Bunu yapmam sebebimiz Rastgele 50 şarkı seçmek ama order by rand yapınca çok uzun sürüyor.
+        # O yüzden en mantıklısı bu şekilde yapmak
+        import random
+        random_ids = random.sample(range(1, total_records + 1), min(50, total_records))
+        
+        # Bu ID'leri kullanarak sorguyu çalıştır
+        query = """
+        SELECT
+            bp_track.track_id,
+            bp_track.title AS song_title,
+            bp_artist.artist_name AS artist_name
+        FROM
+            bp_track
+        LEFT JOIN
+            artist_track ON bp_track.track_id = artist_track.track_id
+        LEFT JOIN
+            bp_artist ON artist_track.artist_id = bp_artist.artist_id
+        LEFT JOIN
+            bp_release ON bp_track.release_id = bp_release.release_id
+        LEFT JOIN 
+            bp_genre ON bp_track.genre_id = bp_genre.genre_id
+        WHERE bp_track.track_id IN ({})
+        """.format(','.join(map(str, random_ids)))
+        cursor.execute(query)
+        songs_list = cursor.fetchall()  # Fetch all results
+    # Render template with fetched songs
+    return render_template('home.html', selected_page=selected_page, songs=songs_list)
+
 
 @main_bp.route('/albums')
 def albums():
