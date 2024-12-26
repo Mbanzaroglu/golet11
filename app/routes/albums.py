f1d94a917c5620df1a0fe90f1b6559e61b6589a4
Umut Önal <onalu20@itu.edu.tr>
2024-12-26 13:13:55 +0300

    sorgu şarkı detaylarına gidebilecek şekilde güncellendi, favoriler kısmı düzenlendi

--- app/routes/albums.py
+++ app/routes/albums.py
@@ -17,6 +17,7 @@ def album_detail(release_id, artist_id):
             br.release_date,
             br.release_title,
             ba.artist_name,
+            ba.artist_id,
             bt.title,
             bt.track_id
         FROM
@@ -49,38 +50,42 @@ def album_detail(release_id, artist_id):
             for row in album_rows if row['title']
         ]
 
+        artist_ids = [
+            row['artist_id'] for row in album_rows if row['artist_id']
+        ]
+
         release_dict = {
             "id": release_data['release_id'],
             "title": release_data['release_title'],
             "artists": [release_data['artist_name']],
+            "artist_ids": artist_ids,  # Sanatçı ID'leri burada liste olarak döner
             "release_date": release_data['release_date'].strftime('%Y-%m-%d') if release_data['release_date'] else "",
             "release_title": release_data['release_title'] if release_data['release_title'] else "Unknown",
             "tracks": tracks
         }
 
-        cursor.execute("""
-            SELECT * FROM favorite_albums 
-            WHERE user_id = %s AND album_id = %s
-        """, (current_user.id, release_id))
-        is_favorite = cursor.fetchone() is not None
+        # Favori kontrolü (sadece oturum açmış kullanıcılar için)
+        is_favorite = False
+        if current_user.is_authenticated:
+            cursor.execute("""
+                SELECT * FROM favorite_albums 
+                WHERE user_id = %s AND album_id = %s
+            """, (current_user.id, release_id))
+            is_favorite = cursor.fetchone() is not None
 
         return render_template('album.html', album=release_dict, is_favorite=is_favorite)
 
 @album_bp.route('/add_favorite_album', methods=['POST'])
 @login_required
 def add_favorite_album():
-    # Kullanıcıdan gelen release_id verisini al
     release_id = request.form.get('release_id')
-
-    print(f"Gönderilen album_id: {release_id}")
 
     if not release_id:
         flash('Geçerli bir albüm seçilmedi.', 'danger')
-        return redirect(url_for('main_bp.home'))  # Kullanıcıyı ana sayfaya yönlendir
-
+        return redirect(url_for('main_bp.home'))
+    
     try:
         with get_db_connection_and_cursor() as (conn, cursor):
-            # Kullanıcının aynı albümü daha önce ekleyip eklemediğini kontrol et
             cursor.execute("""
                 SELECT * FROM favorite_albums WHERE user_id = %s AND album_id = %s
             """, (current_user.id, release_id))
@@ -88,21 +93,17 @@ def add_favorite_album():
             existing_favorite = cursor.fetchone()
             if existing_favorite:
                 flash('Bu albüm zaten favorilerde.', 'info')
-                return redirect(request.referrer or url_for('main_bp.home'))  # Kullanıcıyı önceki sayfaya döndür
+                return redirect(request.referrer or url_for('main_bp.home'))
 
-            # Şarkıyı favorilere ekle
             cursor.execute("""
                 INSERT INTO favorite_albums (user_id, album_id) VALUES (%s, %s)
             """, (current_user.id, release_id))
             conn.commit()
             flash('Albüm favorilere eklendi!', 'success')
-            return redirect(request.referrer or url_for('main_bp.home'))  # Kullanıcıyı önceki sayfaya döndür
-
-
+            return redirect(request.referrer or url_for('main_bp.home'))
     except Exception as e:
         flash(f'Favorilere eklerken bir hata oluştu: {str(e)}', 'danger')
-
-    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+        return redirect(url_for('main_bp.home'))
 
 @album_bp.route('/remove_favorite_album', methods=['POST'])
 @login_required
